8d638c8246bea52729386f186ab95c041891e909
yasin <yascanary0@gmail.com>
2023-04-05 17:03:24 +0330

    continue and goto loop label

--- loopLabel.go
+++ loopLabel.go
@@ -2,10 +2,18 @@ package main
 
 func main() {
 	TestLoop:
+	// the main loop
 	for {
 		for {
 			for {
+				// NOTE: breaks the main loop
 				break TestLoop
+
+				// NOTE: continues the main loop
+				// continue TestLoop
+
+				// NOTE: restarts the main loop
+				// goto TestLoop
 			}
 		}
 	}
