52665ff6ae68cde1ed19e491def45baa59ee171b
yasin <yascanary0@gmail.com>
2023-04-19 15:12:36 +0330

    sub command in go cli

---
+++ cli/subCommand.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fooCmd := flag.NewFlagSet("foo", flag.ExitOnError)
+	fooEnable := fooCmd.Bool("enable", false, "enable the foo")
+	fooName := fooCmd.String("name", "mamad", "change the name of the foo")
+
+	barCmd := flag.NewFlagSet("bar", flag.ExitOnError)
+	barLevel := barCmd.Int("level", 0, "change the level of the bar")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'foo' or 'bar' subcommand")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "foo":
+		fooCmd.Parse(os.Args[2:])
+		fmt.Println("subcommand:", "foo")
+		fmt.Println("enable:", *fooEnable)
+		fmt.Println("name:", *fooName)
+	case "bar":
+		barCmd.Parse(os.Args[2:])
+		fmt.Println("subcommand:", "bar")
+		fmt.Println("level:", *barLevel)
+
+	default:
+		fmt.Println("expected 'foo' or 'bar' subcommands")
+		os.Exit(1)
+	}
+}
